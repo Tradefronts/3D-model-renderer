0008ef95be810bce13cf8b2a75aac6aa6cec4ebc
SubhashPrasad23 <SubhashPrasad027@gmail.com>
2026-01-29 21:10:07 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import FBXViewer from "@/components/FbxRender";
-import ObjViewer from "@/components/ObjRender";
 
 export default function Home() {
   return (
     <div className="h-screen bg-gray-500">
       <FBXViewer/>
-      {/* <ObjViewer/> */}
     </div>
   );
 }
